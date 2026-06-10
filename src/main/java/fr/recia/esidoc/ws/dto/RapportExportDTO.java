624f8cee51e52c980891f5c8deca09ffbe650366
Ewann ROUX <ewann.roux@recia.fr>
2026-06-10 14:05:03 +0200

    feat: create controller and response DTO

---
+++ src/main/java/fr/recia/esidoc/ws/dto/RapportExportDTO.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright © 2026 GIP-RECIA (https://www.recia.fr/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fr.recia.esidoc.ws.dto;
+
+import fr.recia.esidoc.ws.model.RapportExport;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RapportExportDTO {
+
+    public RapportExportDTO(RapportExport rapportExport){
+        this.warnings = rapportExport.getWarnings();
+        this.failure = rapportExport.isFailure();
+        this.failureReason = rapportExport.getFailureReason();
+    }
+
+    List<String> warnings = new ArrayList<>();
+
+    boolean failure;
+    String failureReason;
+
+
+}
---
+++ src/main/java/fr/recia/esidoc/ws/web/rest/ExportEsidocController.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright © 2026 GIP-RECIA (https://www.recia.fr/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fr.recia.esidoc.ws.web.rest;
+
+import fr.recia.esidoc.ws.dto.RapportExportDTO;
+import fr.recia.esidoc.ws.model.RapportExport;
+import fr.recia.esidoc.ws.service.export.IExportEsidocService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequestMapping("/api")
+public class ExportEsidocController {
+
+
+    @Autowired
+    IExportEsidocService exportEsidocService;
+
+    @Autowired
+    RapportExport rapportExport;
+
+
+    @GetMapping(value = "/export/{uai}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<RapportExportDTO> healthCheck(@PathVariable String uai  ) {
+        exportEsidocService.exportAnnuaireForUai(uai);
+        return ResponseEntity.ok().body(new RapportExportDTO(rapportExport));
+    }
+
+}
